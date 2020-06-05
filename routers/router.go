a0a6a5edc80abe42a0ae2f5fff7e4195cbd0a165
lxt <liangliang.too@gmail.com>
2020-06-05 16:11:54 +0800

    unfinish article part

--- routers/router.go
+++ routers/router.go
@@ -23,6 +23,8 @@ func InitRouter() *gin.Engine {
 
 	apiv1 := r.Group("/api/v1")
 	{
+		/* 标签方面 */
+
 		// 获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
 		// 新建标签
@@ -31,6 +33,19 @@ func InitRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		// 删除指定标签
 		apiv1.DELETE("tags/:id", v1.DeleteTag)
+
+		/* 文章方面 */
+
+		// 获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		// 获取指定文章
+		apiv1.GET("/article", v1.GetArticle)
+		// 新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		// 更新指定文章
+		apiv1.PUT("/article/:id", v1.EditArticle)
+		// 删除指定文章
+		apiv1.DELETE("/article/:id", v1.DeleteArticle)
 	}
 
 	return r
