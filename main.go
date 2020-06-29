fb73d5980083e5213587925de147297ca66c19ba
lxt <liangliang.too@gmail.com>
2020-06-29 21:02:56 +0800

    http-server-shutdown test

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"gin.example/pkg/setting"
 	"gin.example/routers"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main()  {
 	router := routers.InitRouter()
-
-
 
 	s := &http.Server{
 		Addr:              fmt.Sprintf(":%d", setting.HTTPPort),
@@ -20,6 +23,27 @@ func main()  {
 		MaxHeaderBytes: 	1 << 20,
 	}
 
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
 	// 开始监听
-	s.ListenAndServe()
+	//s.ListenAndServe()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Serve ... ")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+
+	log.Println("Server exitting")
 }
